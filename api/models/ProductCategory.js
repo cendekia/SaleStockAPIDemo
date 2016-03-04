6d19cbaf0017f81e4671411244f09a1b351c836d
kia <cendiqkrn@gmail.com>
2016-03-04 19:23:47 +0700

    add product categories table

---
+++ api/models/ProductCategory.js
@@ -0,0 +1,38 @@
+/**
+* ProductCategory.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  tableName: 'product_categories',
+
+  attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },
+    productId: {
+      type: 'integer',
+      required: true,
+      columnName: 'product_id',
+      model: 'Product',
+    },
+    categoryId: {
+      type: 'integer',
+      required: true,
+      columnName: 'category_id',
+      model: 'Category',
+    },
+    subCategoryId: {
+      type: 'string',
+      required: false,
+      columnName: 'sub_category_id',
+      model: 'SubCategory'
+    }
+  }
+};
+
