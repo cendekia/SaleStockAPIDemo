180c17afc9e997b86bbdeb0803415ef5879151dc
kia <cendiqkrn@gmail.com>
2016-03-04 20:53:40 +0700

    add product sub category pivot table

---
+++ api/controllers/ProductSubCategoryController.js
@@ -0,0 +1,11 @@
+/**
+ * ProductSubCategoryController
+ *
+ * @description :: Server-side logic for managing productsubcategories
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/ProductSubCategory.js
@@ -0,0 +1,32 @@
+/**
+* ProductSubCategory.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  tableName: 'product_sub_categories',
+
+  attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },
+    productId: {
+      type: 'integer',
+      required: true,
+      columnName: 'product_id',
+      model: 'Product',
+    },
+    categorySubId: {
+      type: 'integer',
+      required: true,
+      columnName: 'category_sub_id',
+      model: 'SubCategory',
+    }
+  }
+};
+
